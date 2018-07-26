56bb29bc89eaa64e9e9387e0d9c2ef8a50da06a0
Max Grønlund <max@synthmax.dk>
2018-07-26 16:48:35 +0200

    bounce back link fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.site_url = "http://localhost:3000/"
 end
